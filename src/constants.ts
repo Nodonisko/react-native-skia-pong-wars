aaf7f6b62cc002a58746227b21201418394b2c61
Daniel Suchy <suchydan@gmail.com>
2024-02-05 17:41:29 +0100

    higher speed

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 export const BRICK_SIZE = 20;
 export const BALL_SIZE = 10;
-export const SPEED = 0.04;
+export const SPEED = 0.045;
 
 export const NUM_BRICKS_PER_ROW = SCREEN_WIDTH / BRICK_SIZE;
 export const NUM_OF_ROWS = SCREEN_HEIGHT / BRICK_SIZE;
